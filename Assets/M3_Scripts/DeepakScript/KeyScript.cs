2ae906a9edbcc8876cd49d8a56f1b0c43aa78f23
daCoolestNerd <bsawyer@gatech.edu>
2016-08-03 14:33:58 -0400

    Key and Door refinement
    door glitch fixed
    
    Key rotates
    
    need to make prefab

--- Assets/M3_Scripts/DeepakScript/KeyScript.cs
+++ Assets/M3_Scripts/DeepakScript/KeyScript.cs
@@ -4,15 +4,36 @@ using System.Collections;
 public class KeyScript : MonoBehaviour {
 
     public GameObject doorObject;
-	// Use this for initialization
+	GameObject redLight;
+	GameObject greenLight;
+
+	public float spinSpeed = 1.0f;
+
+	void Start () {
+		
+		redLight = GameObject.FindGameObjectWithTag("RedLight");
+		greenLight = GameObject.FindGameObjectWithTag("GreenLight");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up*spinSpeed*Time.deltaTime);
+	}
+
 	void OnTriggerEnter (Collider col) {
-        GameObject redLight = GameObject.FindGameObjectWithTag("RedLight");
-        GameObject greenLight = GameObject.FindGameObjectWithTag("GreenLight");
-        redLight.GetComponent<Light>().enabled = false;
-        greenLight.GetComponent<Light>().enabled = true;
-        this.GetComponent<Renderer>().enabled = false;
-        this.GetComponent<Light>().enabled = false;
-        doorObject.GetComponent<doorScript>().isUnlocked = true;
+
+		if (col.gameObject.CompareTag ("Player")) {
+
+			redLight.GetComponent<Light>().enabled = false;
+			greenLight.GetComponent<Light>().enabled = true;
+
+			//this.GetComponent<Renderer>().enabled = false;
+			//this.GetComponent<Light>().enabled = false;
+
+			doorObject.GetComponent<doorScript>().isUnlocked = true;
+			Destroy(this.gameObject);			
+		}
+        
     }
 	
 }
