4b94a84001c272c5f8f4c8872e48967258e0bde4
Deepak Ravindran <dpkravi@gmail.com>
2016-08-03 17:32:22 -0400

    Cleaned up Level0 and Adv Level

--- Assets/M3_Scripts/DeepakScript/KeyScript.cs
+++ Assets/M3_Scripts/DeepakScript/KeyScript.cs
@@ -4,36 +4,15 @@ using System.Collections;
 public class KeyScript : MonoBehaviour {
 
     public GameObject doorObject;
-	GameObject redLight;
-	GameObject greenLight;
-
-	public float spinSpeed = 1.0f;
-
-	void Start () {
-		
-		redLight = GameObject.FindGameObjectWithTag("RedLight");
-		greenLight = GameObject.FindGameObjectWithTag("GreenLight");
-	}
-
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate(Vector3.up*spinSpeed*Time.deltaTime);
-	}
-
+	// Use this for initialization
 	void OnTriggerEnter (Collider col) {
-
-		if (col.gameObject.CompareTag ("Player")) {
-
-			redLight.GetComponent<Light>().enabled = false;
-			greenLight.GetComponent<Light>().enabled = true;
-
-			//this.GetComponent<Renderer>().enabled = false;
-			//this.GetComponent<Light>().enabled = false;
-
-			doorObject.GetComponent<doorScript>().isUnlocked = true;
-			Destroy(this.gameObject);			
-		}
-        
+        GameObject redLight = GameObject.FindGameObjectWithTag("RedLight");
+        GameObject greenLight = GameObject.FindGameObjectWithTag("GreenLight");
+        redLight.GetComponent<Light>().enabled = false;
+        greenLight.GetComponent<Light>().enabled = true;
+        this.GetComponent<Renderer>().enabled = false;
+        this.GetComponent<Light>().enabled = false;
+        doorObject.GetComponent<doorScript>().isUnlocked = true;
     }
 	
 }
--- Assets/rotatePill.cs
+++ Assets/rotatePill.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class rotatePill : MonoBehaviour {
 
-	public float spinSpeed = 1.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +10,6 @@ public class rotatePill : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(new Vector3(15,30,15)*spinSpeed*Time.deltaTime);
+		transform.Rotate(Vector3.right * 45*Time.deltaTime);
 	}
 }
